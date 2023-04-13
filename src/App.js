77403df763903e2a812c0f5196db43a75bc6e09f
Hygor Vinicius <hv-pm@hotmail.com>
2023-04-13 11:45:56 -0300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+import ProfileCards from "./ProfileCards";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+      <div className="hero-body">
+        <p className="title">Personal Digital Assistance</p>
+      </div>
+    </section>
+
+      <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-4">
+                <ProfileCards 
+                title="Alexa" 
+                handle="@alexa99" 
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things!"
+                />
+              </div>
+              <div className="column is-4">
+                <ProfileCards 
+                title="Cortana" 
+                handle="@cortana32" 
+                image={CortanaImage}
+                description="Cortana was created by Microsoft. Who knows what it does?"
+                />
+              </div>
+              <div className="column is-4">
+                <ProfileCards 
+                title="Siri" 
+                handle="@siri01" 
+                image={SiriImage}
+                description="Siri was created by Apple. She's funny."
+                />
+              </div>
+            </div>
+          </section>
+      </div>
     </div>
   );
 }
